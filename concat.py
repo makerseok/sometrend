d2c9d54bb7ff946eab6144480a0e3376e36062c5
makerseok <maker7788@gmail.com>
2021-08-17 11:00:24 +0900

    work all datas in root_dir

--- concat.py
+++ concat.py
@@ -1,43 +1,58 @@
-import pandas as pd
-import pickle
 import os
+from glob import glob
+import pickle
+
+import pandas as pd
 
 
-def get_excel_files(target_dir, data):
-    paths = os.listdir(target_dir)
-    for path in paths:
-        rawdata = pd.read_excel(os.path.join(target_dir, path), header=14)
+def get_excel_files(root_dir, data, target_dir="**", get_all=False):
+    files = glob(f"./{root_dir}/{target_dir}/*.xlsx", recursive=True)
+    print(files)
+    for file in files:
+        rawdata = pd.read_excel(file, header=14)
         name = rawdata.loc[:, rawdata.columns.str.startswith("연관어")]
         num = rawdata.loc[:, rawdata.columns.str.startswith("건수")]
-        data.append([name, num])
+        if get_all:
+            data.append([name.iloc[:, -1], num.iloc[:, -1]])
+        else:
+            data.append([name.iloc[:, :-1], num.iloc[:, :-1]])
     return data
 
 
 def df_to_list(name, num, lists):
     for i in range(name.shape[1]):
-        temp = [
-            (na + " ") * int(nu)
-            for na, nu in zip(name.iloc[:, i], num.iloc[:, i])
-            if pd.notnull(nu)
-        ]
-        print(temp)
-        name_string = " ".join(temp).strip()
-        lists.append(name_string)
+        temp = [[na] * int(nu) for na, nu in zip(name.iloc[:, i], num.iloc[:, i]) if pd.notnull(nu)]
+        name_list = sum(temp, [])
+        lists.append(name_list)
     return lists
 
 
+def save_pickle(base_dir, num_files, data, is_all=False):
+    filename = f"files-{num_files}"
+    if is_all:
+        filename += "_all"
+    else:
+        filename += f"_rows-{len(data)}"
+    filepath = os.path.join(base_dir, filename)
+    with open(filepath, "wb") as f:
+        pickle.dump(data, f)
+    print(filepath, "saved")
+
+
 if __name__ == "__main__":
-    target_dir = "./rawdata/아모레퍼시픽_선크림"
+    root_dir = "./rawdata"
     data = []
-    data = get_excel_files(target_dir, data)
+    data_all = []
+    data = get_excel_files(root_dir, data)
+    data_all = get_excel_files(root_dir, data_all, get_all=True)
+
+    num_files = len(data)
+    print(num_files)
+
     lists = []
     for name, num in data:
         lists = df_to_list(name, num, lists)
-    print(lists)
-    for l in lists:
-        print(len(l))
-    filename = target_dir.split("/")[-1]
-    filepath = os.path.join("./listdata", filename)
-    with open(filepath, "wb") as f:
-        pickle.dump(lists, f)
-    print(filepath, "saved")
+    print(len(lists))
+
+    save_pickle("./listdata", num_files, lists)
+    save_pickle("./listdata", num_files, data_all, is_all=True)
